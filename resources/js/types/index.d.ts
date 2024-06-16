bd7c6db07d0b3a7c3d7f319b6e5e7fd31423f159
AlfredPaguio <alfredpaguio36@gmail.com>
2024-06-16 23:11:19 +0800

    Add types of Project and Public User

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,12 +1,31 @@
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    email_verified_at: string;
+  id: number;
+  name: string;
+  email: string;
+  email_verified_at: string;
 }
 
-export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
-    auth: {
-        user: User;
-    };
+export type PublicUser = Omit<User, "email_verified_at">;
+
+export interface Project {
+  id: number;
+  name: string;
+  description: string;
+  image_path: string;
+  created_at: Date;
+  due_date: Date;
+  status: string;
+  createdBy: PublicUser;
+  updatedBy: PublicUser;
+}
+
+export type PageProps<
+  T extends Record<string, unknown> = Record<string, unknown>
+> = T & {
+  auth: {
+    user: User;
+  };
+  projects: {
+    data: Project[];
+  };
 };
