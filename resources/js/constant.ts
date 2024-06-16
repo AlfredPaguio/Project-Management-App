9ce8729a47f1e43d93d5b26e1c34d9385e3a80a7
AlfredPaguio <alfredpaguio36@gmail.com>
2024-06-16 23:38:45 +0800

    Add Task resource placeholder

---
+++ resources/js/constant.ts
@@ -0,0 +1,11 @@
+export enum Status_Enum {
+  Completed = "completed",
+  InProgress = "in_progress",
+  Pending = "pending",
+}
+
+export const STATUS = {
+  completed: "Completed",
+  in_progress: "In Progress",
+  pending: "Pending",
+};
