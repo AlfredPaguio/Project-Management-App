9e453ac1ad754dabe6792519ea0992ab1d9084f4
AlfredPaguio <alfredpaguio36@gmail.com>
2024-07-14 18:08:08 +0800

    Setup project creation form
    need to fix form schema shape

---
+++ resources/js/utils/getImageData.ts
@@ -0,0 +1,19 @@
+import { ChangeEvent } from "react";
+
+export function getImageData(event: ChangeEvent<HTMLInputElement>) {
+  // FileList is immutable, so we need to create a new one
+  const dataTransfer = new DataTransfer();
+
+  // Add newly uploaded images
+  Array.from(event.target.files!).forEach((image) =>
+    dataTransfer.items.add(image)
+  );
+
+  const files = dataTransfer.files;
+  const displayUrl = URL.createObjectURL(event.target.files![0]);
+
+  return { files, displayUrl };
+}
+
+// thank you very much
+// https://github.com/shadcn-ui/ui/issues/250#issuecomment-1749174706
