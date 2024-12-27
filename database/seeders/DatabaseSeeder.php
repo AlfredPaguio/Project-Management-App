9ca834df8ee976681beb09fba7ac30af05a17777
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2024-12-28 03:31:05 +0800

    Add Roles and Permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,9 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -22,6 +25,28 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('test12345'),
             'email_verified_at' => now(),
         ]);
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'manager']);
+        Role::create(['name' => 'user']);
+
+        Permission::create(['name' => 'view tasks']);
+        Permission::create(['name' => 'edit tasks']);
+
+        Permission::create(['name' => 'manage-projects']);
+        Permission::create(['name' => 'manage-users']);
+
+        $adminRole = Role::findByName('admin');
+        $adminRole->givePermissionTo(['view tasks', 'edit tasks', 'manage-projects', 'manage-users']);
+
+        $managerRole = Role::findByName('manager');
+        $managerRole->givePermissionTo(['view tasks', 'edit tasks', 'manage-projects']);
+
+        $userRole = Role::findByName('user');
+        $userRole->givePermissionTo(['view tasks']);
+
+        $user = User::find(1);
+        $user->assignRole('admin');
 
         Project::factory()->count(100)->hasTasks(10)->create();
     }
--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,26 @@ Route::middleware(['auth', 'verified'])->group(function () {
         [DashboardController::class, 'index']
     )->name('dashboard');
 
-    Route::resource('project', ProjectController::class);
-    Route::get('/task/my-tasks', [TaskController::class, 'myTasks'])
-        ->name('task.myTasks');
-    Route::resource('task', TaskController::class);
-    Route::resource('user', UserController::class);
+    Route::middleware(['role:admin|manager', 'can:manage-projects'])->group(function () {
+        Route::resource('project', ProjectController::class);
+    });
+
+    Route::middleware(['can:view-tasks'])->group(function () {
+        Route::get('/task/my-tasks', [TaskController::class, 'myTasks'])
+            ->name('task.myTasks');
+
+        Route::resource('task', TaskController::class);
+    });
+
+    Route::middleware(['role:admin', 'can:manage-users'])->group(function () {
+        Route::resource('user', UserController::class);
+    });
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('auth')->prefix('profile')->name('profile.')->group(function () {
+    Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+    Route::patch('/', [ProfileController::class, 'update'])->name('update');
+    Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
 });
 
 require __DIR__ . '/auth.php';
