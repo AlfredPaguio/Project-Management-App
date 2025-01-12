0309b4df4e778736eda7efcb43daef1dd301520e
Alfred Umali Paguio <alfredpaguio36@gmail.com>
2025-01-12 12:34:17 +0800

    Update User Create Page
    to reflect changes on Edit Page

--- resources/js/Pages/User/Create.tsx
+++ resources/js/Pages/User/Create.tsx
@@ -2,58 +2,22 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PageProps } from "@/types";
 import { Head, Link, router } from "@inertiajs/react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar";
 import { Button } from "@/Components/ui/button";
-import { Card } from "@/Components/ui/card";
-import { Checkbox } from "@/Components/ui/checkbox";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/Components/ui/card";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  Form
 } from "@/Components/ui/form";
-import { Input } from "@/Components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/Components/ui/tabs";
 import { Permission, Role } from "@/types/user";
-import { getImageData } from "@/utils/getImageData";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ArrowLeft } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
-const formSchema = z
-  .object({
-    name: z.string().max(255),
-    email: z.string().email(),
-    password: z
-      .string()
-      .min(8, "Password must be at least 8 characters")
-      .regex(/[A-Za-z]/, "Password must contain at least one letter")
-      .regex(/\d/, "Password must contain at least one digit")
-      .regex(
-        /[^A-Za-z0-9]/,
-        "Password must contain at least one special character"
-      )
-      // the optional doesn't work because zod doesn't treat empty string as null or undefined
-      // so it's against conditions above (.min)
-      .or(z.literal(""))
-      .optional(),
-    confirmPassword: z
-      .string()
-      .min(8)
-      .or(z.literal("")) //pls work
-      .optional(),
-    avatar: z.instanceof(File).optional(),
-    roles: z.array(z.number()),
-    permissions: z.array(z.number()),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords don't match",
-    path: ["confirmPassword"],
-  });
+import PermissionsSelection from "./partials/PermissionsSelection";
+import RolesSelection from "./partials/RolesSelection";
+import UserDetailsForm from "./partials/UserDetailsForm";
+import { FormDataType, formSchema } from "./schema/formSchema";
 
 interface UserCreatePageProps {
   roles: Role[];
@@ -67,7 +31,7 @@ export default function Create({
 }: PageProps & UserCreatePageProps) {
   const [preview, setPreview] = useState("");
 
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<FormDataType>({
     mode: "onSubmit",
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -94,12 +58,18 @@ export default function Create({
     if (values.avatar) {
       formData.append("avatar", values.avatar);
     }
-    values.roles.forEach((roleId) =>
-      formData.append("roles[]", roleId.toString())
-    );
-    values.permissions.forEach((permissionId) =>
-      formData.append("permissions[]", permissionId.toString())
-    );
+
+    if (values.roles) {
+      values.roles.forEach((roleId) =>
+        formData.append("roles[]", roleId.toString())
+      );
+    }
+
+    if (values.permissions) {
+      values.permissions.forEach((permissionId) =>
+        formData.append("permissions[]", permissionId.toString())
+      );
+    }
 
     router.post(route("user.store"), formData);
   }
@@ -127,224 +97,44 @@ export default function Create({
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <Card className="overflow-hidden shadow-sm sm:rounded-lg">
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="p-4 space-y-8"
-              >
-                <div className="flex items-center space-x-4">
-                  <Avatar className="w-24 h-24">
-                    <AvatarImage
-                      src={preview || undefined}
-                      alt={"Avatar Preview"}
-                    />
-                    <AvatarFallback>
-                      {auth.user.name.charAt(0).toUpperCase()}
-                    </AvatarFallback>
-                  </Avatar>
-                  <FormField
-                    control={form.control}
-                    name="avatar"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Avatar</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="file"
-                            accept="image/*"
-                            onChange={(e) => {
-                              const { files, displayUrl } = getImageData(e);
-                              setPreview(displayUrl);
-                              field.onChange(files ? files[0] : null);
-                            }}
-                          />
-                        </FormControl>
-                        <FormDescription>
-                          Upload a new profile picture (optional).
-                        </FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="User Name..." {...field} />
-                      </FormControl>
-                      <FormDescription>This is your User name.</FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Email..." {...field} type="email" />
-                      </FormControl>
-                      <FormDescription>
-                        This is your User Email.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Password..."
-                          {...field}
-                          type="password"
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        This is your User password.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="confirmPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Confirm Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Confirm Password..."
-                          {...field}
-                          type="password"
-                        />
-                      </FormControl>
-                      <FormDescription>
-                        Please confirm your password.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="roles"
-                  render={() => (
-                    <FormItem>
-                      <FormLabel>Roles</FormLabel>
-                      <div className="space-y-2">
-                        {roles.map((role) => (
-                          <FormField
-                            key={role.id}
-                            control={form.control}
-                            name="roles"
-                            render={({ field }) => {
-                              return (
-                                <FormItem
-                                  key={role.id}
-                                  className="flex flex-row items-start space-x-3 space-y-0"
-                                >
-                                  <FormControl>
-                                    <Checkbox
-                                      checked={field.value?.includes(role.id)}
-                                      onCheckedChange={(checked) => {
-                                        return checked
-                                          ? field.onChange([
-                                              ...field.value,
-                                              role.id,
-                                            ])
-                                          : field.onChange(
-                                              field.value?.filter(
-                                                (value) => value !== role.id
-                                              )
-                                            );
-                                      }}
-                                    />
-                                  </FormControl>
-                                  <FormLabel className="font-normal">
-                                    {role.name}
-                                  </FormLabel>
-                                </FormItem>
-                              );
-                            }}
-                          />
-                        ))}
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="permissions"
-                  render={() => (
-                    <FormItem>
-                      <FormLabel>Permissions</FormLabel>
-                      <div className="space-y-2">
-                        {permissions.map((permission) => (
-                          <FormField
-                            key={permission.id}
-                            control={form.control}
-                            name="permissions"
-                            render={({ field }) => {
-                              return (
-                                <FormItem
-                                  key={permission.id}
-                                  className="flex flex-row items-start space-x-3 space-y-0"
-                                >
-                                  <FormControl>
-                                    <Checkbox
-                                      checked={field.value?.includes(
-                                        permission.id
-                                      )}
-                                      onCheckedChange={(checked) => {
-                                        return checked
-                                          ? field.onChange([
-                                              ...field.value,
-                                              permission.id,
-                                            ])
-                                          : field.onChange(
-                                              field.value?.filter(
-                                                (value) =>
-                                                  value !== permission.id
-                                              )
-                                            );
-                                      }}
-                                    />
-                                  </FormControl>
-                                  <FormLabel className="font-normal">
-                                    {permission.name}
-                                  </FormLabel>
-                                </FormItem>
-                              );
-                            }}
-                          />
-                        ))}
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <Button type="button" variant={"destructive"} asChild>
-                  <Link href={route("user.index")}>Cancel</Link>
-                </Button>
-                <Button type="submit">Submit</Button>
-              </form>
-            </Form>
+            <CardHeader>
+              <CardTitle>Create New User</CardTitle>
+              <CardDescription>Enter details to create a new user</CardDescription>
+            </CardHeader>
+            <CardContent className="p-0">
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="p-4 space-y-8"
+                >
+                  <Tabs defaultValue="details" className="w-full">
+                    <TabsList className="grid w-full grid-cols-3">
+                      <TabsTrigger value="details">User Details</TabsTrigger>
+                      <TabsTrigger value="roles">Roles</TabsTrigger>
+                      <TabsTrigger value="permissions">Permissions</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="details" className="space-y-4">
+                      <UserDetailsForm form={form} />
+                    </TabsContent>
+                    <TabsContent value="roles" className="space-y-4">
+                      <RolesSelection form={form} roles={roles} />
+                    </TabsContent>
+                    <TabsContent value="permissions" className="space-y-4">
+                      <PermissionsSelection
+                        form={form}
+                        permissions={permissions}
+                      />
+                    </TabsContent>
+                  </Tabs>
+                  <CardFooter className="flex justify-between">
+                    <Button type="button" variant={"destructive"} asChild>
+                      <Link href={route("user.index")}>Cancel</Link>
+                    </Button>
+                    <Button type="submit">Submit</Button>
+                  </CardFooter>
+                </form>
+              </Form>
+            </CardContent>
           </Card>
         </div>
       </div>
